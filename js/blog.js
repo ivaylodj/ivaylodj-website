543f40caca2bf30487760d30c1805ae3fdb96061
Ivaylo Djounov <ivaylo.djounov@gmail.com>
2026-07-11 19:02:10 +0300

    fix: use comma delimiter for multiple categories in post meta
    - Change category separator from space to comma
    - Multiple categories now display as: Category1, Category2

--- js/blog.js
+++ js/blog.js
@@ -205,7 +205,7 @@
       if (post.categories && post.categories.length > 0) {
         metaHtml += '<div class="cherga_post_meta_item">';
         for (var j = 0; j < post.categories.length; j++) {
-          if (j > 0) metaHtml += ' ';
+          if (j > 0) metaHtml += ', ';
           metaHtml += '<a href="javascript:void(0)" rel="category tag" onclick="filterByCategory(\'' + post.categories[j] + '\');">' + post.categories[j] + '</a>';
         }
         metaHtml += '</div>';
